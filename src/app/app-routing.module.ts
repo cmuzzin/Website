d86b1ea1e7c1df4497932ac106538d76ba9c3c60
Christopher Muzzin <chrismuzzin@christophersmbp.attlocal.net>
2018-04-19 21:58:46 -0500

    click header and router fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {PortfolioComponent} from "./portfolio/portfolio.component";
 import {HomeComponent} from "./home/home.component";
+import {AppComponent} from "./app.component";
 
 const routes: Routes = [
   {
@@ -16,6 +17,10 @@ const routes: Routes = [
   {
     component: HomeComponent,
     path:'home'
+  },
+  {
+    component: AppComponent,
+    path:'**'
   }
 ];
 
