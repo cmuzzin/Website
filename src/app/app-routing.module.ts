4834ff694ceb8f35cd38b156851628b8991a8162
Chris Muzzin <cmuzzin@uark.edu>
2018-08-27 18:33:56 -0500

    brought back the portfolio page for development

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {HomeComponent} from "./home/home.component";
+import {PortfolioComponent} from "./portfolio/portfolio.component";
 
 const routes: Routes = [
   {
@@ -11,6 +12,10 @@ const routes: Routes = [
   {
     component: HomeComponent,
     path:'home'
+  },
+  {
+    component: PortfolioComponent,
+    path:'portfolio'
   },
   {
     component: HomeComponent,
