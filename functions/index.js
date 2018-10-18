306b324f458d54373d737f241da8707bf62d2602
Chris Muzzin <dirtymuzz@gmail.com>
2018-10-18 16:02:38 -0500

    updated firebase admin

--- functions/index.js
+++ functions/index.js
@@ -1,24 +1,22 @@
-// The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
 const functions = require('firebase-functions');
-
-// The Firebase Admin SDK to access the Firebase Realtime Database.
 const admin = require('firebase-admin');
-admin.initializeApp(functions.config().firebase);
-const nodemailer = require('nodemailer');
+admin.initializeApp();
 
-const gmailEmail = encodeURIComponent(functions.config().gmail.user);
-const gmailPassword = encodeURIComponent(functions.config().gmail.pass);
+const nodemailer = require('nodemailer');
+const gmailEmail = encodeURIComponent(functions.config().gmail.email);
+const gmailPassword = encodeURIComponent(functions.config().gmail.password);
 const mailTransport = nodemailer.createTransport(`smtps://${gmailEmail}:${gmailPassword}@smtp.gmail.com`);
 
 
-exports.sendContactMessage = functions.database.ref('/messages/{pushKey}').onWrite(event => {
-  const snapshot = event.data;
+exports.sendContactMessage = functions.database.ref('/messages/{pushKey}').onWrite((change, context) => {
 // Only send email for new messages.
-  if (snapshot.previous.val() || !snapshot.val().name) {
+  if (change.before.exists()) {
+    //Do nothing if data is edited or deleted
+    console.log('Message edited or deleted - skip');
     return;
   }
 
-  const val = snapshot.val();
+  const val = change.after.data.val();
 
   const mailOptions = {
     to: 'chrismuzzin1020@gmail.com',
