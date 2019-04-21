00f6ecd2c6d85d2ab65a4c82333421f1a14e0f03
Chris Muzzin <cmuzzin@uark.edu>
2019-04-20 20:50:24 -0500

    hosting images virtually

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDGw-tbecIFPQvlGKivFKiRN_dLOV5npbk',
+    authDomain: 'website-86266.firebaseapp.com',
+    databaseURL: 'https://website-86266.firebaseio.com',
+    projectId: 'website-86266',
+    storageBucket: 'website-86266.appspot.com',
+    messagingSenderId: '670442237891'
+  }
 };
